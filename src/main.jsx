3f3dfcf5f5ae9b410830e531c94584bc482cd126
sebastian <sebastianpedroza442@gmail.com>
2025-03-19 00:08:59 -0500

    Se agrego el maquetado del encabezado y el carrusel a la pantalla principal

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import Encabezado from './Encabezado'
+import Carrusel from './Carrusel'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
-  </StrictMode>,
+    <Encabezado />
+    <Carrusel />
+  </StrictMode>
 )
