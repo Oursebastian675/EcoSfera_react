1c5b565ffc80bdb9ff9d2ca515de1977a00c8842
Isaac-max-bit <garciaalejoisaac@gmail.com>
2025-03-23 18:35:31 -0500

    ✅  update

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from "../assets/logoEcoSfera.png";
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Encabezado() {
     const navigate = useNavigate();
@@ -18,36 +18,58 @@ function Encabezado() {
         <header>
             <div className="contenedor">
                 <div className="cabecera">
-                    <div className="Logo">
-                        <img src={Logo} alt="LogoEcoSfera" />
+                    <div className="logo">
+                        <img src={Logo} alt="Logo EcoSfera" />
                     </div>
-                    <div className="titulo"><p>Eco</p></div>
-                    <div className="titulo2"><span>Sfera</span></div>
+                    <div className="titulo">
+                        <p>Eco</p>
+                    </div>
+                    <div className="titulo2">
+                        <span>Sfera</span>
+                    </div>
 
-                    <button className="cssbuttons-io" onClick={handleSearchClick}>
-                        <span>
-                            Busqueda
-                            <svg viewBox="0 0 19.9 19.7" xmlns="http://www.w3.org/2000/svg" className="search-icon">
-                                <path d="M18.5 18.3l-5.4-5.4" stroke-linecap="square"></path>
-                                <circle r="7" cy="8" cx="8"></circle>
-                            </svg>
-                        </span>
-                    </button>
+                    {/* Buscador */}
+                    <div className="group">
+                        <svg
+                            viewBox="0 0 24 24"
+                            aria-hidden="true"
+                            className="icon"
+                            onClick={handleSearchClick}
+                        >
+                            <g>
+                                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                            </g>
+                        </svg>
+                        <input
+                            className="input"
+                            type="search"
+                            placeholder="Search"
+                            onClick={handleSearchClick} // Optional: Adding search functionality
+                        />
+                    </div>
 
-                    <select className="categorias_opciones" name="categorias" id="categoria">
-                        <option value="">Categorias</option>
+                    {/* Selector de categorías */}
+                    <select
+                        className="categorias_opciones"
+                        name="categorias"
+                        id="categoria"
+                    >
+                        <option value="">Categorías</option>
                         <option value="aseoPersonal">Aseo personal</option>
                         <option value="hogar">Hogar</option>
                         <option value="bienestarYBelleza">Bienestar y belleza</option>
                         <option value="mascotas">Mascotas</option>
                     </select>
 
+                    {/* Botón Blog */}
                     <button className="btn-donate">Blog</button>
 
+                    {/* Botón para ingresar */}
                     <button className="button" onClick={handleLoginClick}>
                         INGRESAR
                     </button>
 
+                    {/* Carrito de compras (vacío por ahora) */}
                     <div className="carrito_compras"></div>
                 </div>
             </div>
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,6 +1,5 @@
 import Encabezado from "../components/Encabezado.jsx"
 import Carrusel from "../components/Carrusel.jsx"
-import Login from "../components/Login.jsx";
 
 function Inicio() {
     return (
